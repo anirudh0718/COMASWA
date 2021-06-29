Rectified the wrong assigned in distance calculation

barrier.py
@@ -4,7 +4,7 @@ import time
 from cvxopt import matrix, solvers
 import functions
 
-K = 0.2
+K = 1
 # Class Defining our CBF control
 
 class ebcf_control:
@@ -64,24 +64,26 @@ class ebcf_control:
     def compute_safe(self,obs,Robots,i,id):
         P = np.array([[2, 0],[0, 2]])
         q = 2*self.compute_nom().reshape(2,)
-        h = 1*self.compute_h(obs,Robots,i,id)
+        h = self.compute_h(obs,Robots,i,id)
+
+        print('This is h',h)
         self.h['h'].append(h)
         self.dist.append(functions.calc_dist(self.state,Robots,i))
         G = self.compute_G(obs,Robots,i,id)
         
-        sol = solve_qp(P,q,G,h)
-        u_st = sol['x']
-        """ try:
+        #sol = solve_qp(P,q,G,h)
+        #u_st = sol['x']
+        try:
             sol = solve_qp(P,q,G,h)
             u_st = sol['x']
         except ValueError:
-            u_st = [0,0] """
+            u_st = matrix([0,0])
         return u_st
 
     # Creates respective arrays for h(x) values to be stored for plotting
     def create_h_arr(self):
         arr_list =[]
-        print('These are number of values in one h(x) array',len(self.h['h'][0]))
+        #print('These are number of values in one h(x) array',len(self.h['h'][0]))
         for i in range(len(self.h['h'][0])):
             arr_list.append(np.zeros((len(self.h['h']),1)))
         return arr_list
@@ -89,7 +91,7 @@ class ebcf_control:
     # Creates respective arrays for h(x) values to be stored for plotting
     def create_d_arr(self):
         arr_list =[]
-        print('These are number of values in one h(x) array',len(self.dist[0]))
+        #print('These are number of values in one distance array',len(self.dist[0]))
         for i in range(len(self.dist[0])):
             arr_list.append(np.zeros((len(self.dist),1)))
         return arr_list
@@ -100,7 +102,7 @@ class ebcf_control:
         plt.xlabel("Time(t)")
         plt.ylabel("h(x)")
         plt.title('Plot of h(x) array with {} values for robot {}'.format(len(self.h['h'][0]),self.id + 1))
-        print(self.h['h'][0])
+        #print(self.h['h'][0])
         for i in range(len(h_list)):
             for j in range(len(self.h['h'])):
                 h_list[i][j] = self.h['h'][j][i]
@@ -115,7 +117,7 @@ class ebcf_control:
         plt.xlabel("Time(t)")
         plt.ylabel("h(x)")
         plt.title('Plot of distance of robot {} w.rt to othe robots'.format(self.id))
-        print('This is the distance of robot {} w.rt to othe robots'.format(self.id),self.dist[0])
+        #print('This is the distance of robot {} w.rt to othe robots'.format(self.id),self.dist[0])
         for i in range(len(d_list)):
             for j in range(len(self.dist)):
                 d_list[i][j] = self.dist[j][i]

formationcontrol.py
@@ -9,14 +9,15 @@ start = []
 #start.append(get_rob_gposes(np.array([0,0])))
 start.append(get_rob_rec_pos(np.array([0,0])))
 
+
 # Goal positions of our robots
 goal = []
 #goal.append(get_rob_gposes(np.array([8,0]))) # sqaure
-goal.append(get_turn_orient(np.array([3,5]))) # Turned Rectangle
+goal.append(get_turn_orient(np.array([20,20]))) # Turned Rectangle
 
 
-print('These are the starting postions of the robots',start)
-print('These are goal position f the robots',goal)
+#print('These are the starting postions of the robots',start)
+#print('These are goal position f the robots',goal)
 
 
 # Inititate our robots
@@ -44,8 +45,8 @@ x_init4 = start[0][3]
 goal_init4 =goal[0][3]
 Robot4 = Robot_Sim(x_init4, goal_init4,3)
 
-const_obs = np.array([[10], [10]])
-const_obs2 = np.array([[-20], [-20]])
+const_obs = np.array([[8], [8]])
+const_obs2 = np.array([[5], [5]])
 
 # These are static obstacles we present to the robot
 obs = np.hstack((const_obs2, const_obs))
@@ -58,7 +59,7 @@ poses = get_pose(Robots)
 
 a, ax1 = plt.subplots()
 
-for tt in range(8000):
+for tt in range(1000):
 
     dxi = np.zeros((2, N))
     safe_dxi = np.zeros((2,N))
@@ -70,7 +71,7 @@ for tt in range(8000):
             IF Id = 5 --> Only Formation lesser case and obstacle avoidance
             Formation greater case = |xi -xj| - (Df - e) > 0"""
         if tt>0:            
-            Robots[i].robot_step(obs,Robots,i,4)
+            Robots[i].robot_step(obs,Robots,i,3)
             
 
     if tt%50 ==0:
@@ -78,7 +79,7 @@ for tt in range(8000):
         plt.cla()
 
         for robot in Robots:
-            plot_step(robot.state_hist,ax1)
+            plot_step(robot.state_hist,ax1,obs)
             
         plt.pause(0.1)
 for robot in Robots:

functions.py
@@ -3,10 +3,20 @@ import matplotlib.pyplot as plt
 import time
 from cvxopt import matrix, solvers
 
-# Distance between the robot and obstacle (minimal)
-Ds = 2 
+# Number of robots
+N = 4
 
-e = 0.05
+# Laplacian matrix for a square/Rectangle shape with 4 robots
+L = np.array([
+    [3, -1, -1, -1],
+    [-1, 2, -1, 0],
+    [-1, -1, 3, -1],
+    [-1, 0 , -1, 2]]) 
+
+# Distance between the robot and obstacle (minimal)
+Ds = 5
+
+e = 0.4
 
 
 # Formation Distance for square shape
@@ -24,10 +34,10 @@ weights = np.array([
 ])
 
 # Formation Distance for rectangle shape
-Df_l = 2
-Df_b = 1
+Df_l = 3
+Df_b = 2
 
-ddiag_rec =np.sqrt(np.power(Df_l,2) + np.power(Df_b,2))
+ddiag_rec =np.sqrt(np.power(Df_l,2) + np.power(Df_b,2))  # 2.2360
 
 weights_rec = np.array([
     [0, Df_b,ddiag_rec,Df_l],
@@ -36,14 +46,8 @@ weights_rec = np.array([
     [Df_l, 0,Df_b, 0]
 ])
 
-# Laplacian matrix for a square shape with 4 robots
-L = np.array([
-    [3, -1, -1, -1],
-    [-1, 2, -1, 0],
-    [-1, -1, 3, -1],
-    [-1, 0 , -1, 2]]) 
-# Number of robots
-N = 4
+
+
 
 def topological_neighbors(L, agent):
     """ Returns the neighbors of a particular agent using the graph Laplacian
@@ -76,38 +80,32 @@ def compute_hobs(obst,state):
         xo = sub[0]
         yo = sub[1]
         h[i] = np.power(xo,2)+ np.power(yo,2)- np.power(Ds,2) 
-    return h
+    return 50*h
 
 # Compute hf for only greater than inequality
 def compute_hf_g(state,Robots,i):
     hf = np.zeros((len(topological_neighbors(L,i)), 1))
-    diff ={}
+    idx = 0    
     for j in topological_neighbors(L,i):
         sub =  state['q'][:2].reshape(2,1) - Robots[j].state['q'][:2].reshape(2,1)
         xo = sub[0]
         yo = sub[1]
             
-        diff[j]= (np.power(xo,2)+ np.power(yo,2))
-
-    for m in range(len(diff.keys())):
-        for key in diff.keys():
-            hf[m] =  diff[key]- np.power(weights_rec[i,key] - e,2)
+        hf[idx]= (np.power(xo,2)+ np.power(yo,2)) - np.power(weights_rec[i,j] - e,2)
+        idx += 1
 
     return hf
 # Compute hf for only lesser than inequality
 def compute_hf_l(state,Robots,i):
     hf = np.zeros((len(topological_neighbors(L,i)), 1))
-    diff ={}
+    idx = 0
     for j in topological_neighbors(L,i):
         sub =  state['q'][:2].reshape(2,1) - Robots[j].state['q'][:2].reshape(2,1)
         xo = sub[0]
         yo = sub[1]
             #print(xo,yo)
-        diff[j]= (np.power(xo,2)+ np.power(yo,2))
-
-    for m in range(len(diff.keys())):
-        for key in diff.keys():
-            hf[m] =  np.power(weights_rec[i,key]+e,2) - diff[key]
+        hf[idx]= np.power(weights_rec[i,j] + e,2) - (np.power(xo,2)+ np.power(yo,2))
+        idx += 1
 
     return hf
 

plotrob.py
@@ -4,22 +4,23 @@ import numpy as np
 
 
 
-def plot_step(state_hist, plot_handle):
+def plot_step(state_hist, plot_handle,new_obs):
     state_hist_plot = np.array(state_hist)
 
-    plot_handle.set_xlim([-5, 5])
-    plot_handle.set_ylim([-5, 5])
+    plot_handle.set_xlim([-10, 10])
+    plot_handle.set_ylim([-10, 10])
 
     plot_handle.plot(state_hist_plot[:, 0], state_hist_plot[:, 1],'k')
     #plot_handle.plot(ecbf.goal[0], ecbf.goal[1], '*r')
     plot_handle.plot(state_hist_plot[-1, 0], state_hist_plot[-1, 1], '8k') # current
-
+    for i in range(new_obs.shape[1]):
+        plot_handle.plot(new_obs[0, i], new_obs[1, i], '8k') # obs
     
 
-    ell = Ellipse((state_hist_plot[-1, 0], state_hist_plot[-1, 1]), 0.3, 0.3, 0)
+    ell = Ellipse((state_hist_plot[-1, 0], state_hist_plot[-1, 1]), 2, 2, 0)
     ell.set_alpha(0.3)
     ell.set_facecolor(np.array([1, 0, 0]))
     
     plot_handle.add_artist(ell)
-    #plot_handle.axis('equal')
+    plot_handle.axis('equal')
     