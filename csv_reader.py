f1685d31302e7e34e33acc889cd00088db0bf16a
lucifer1239 <anirudh.aynala@tuni.fi>
2021-11-29 05:03:57 +0200

    Data for report

--- csv_reader.py
+++ csv_reader.py
@@ -5,7 +5,7 @@ import sys
 
 
 headers = ['h0','h1','h2','h3','h4','h5','h6','h7','h8','h9']
-csv_file = pd.read_csv('h111obs_1form.csv')
+csv_file = pd.read_csv('h312Form.csv')
 
 h0 = csv_file['h0']
 h1 = csv_file['h1']
@@ -20,11 +20,11 @@ h8 = csv_file['h8']
 
 fig, ax1 = plt.subplots()
 #csv_file.plot()
-x = +1.6
+x = 0.16
 
-ax1.plot(h0+x,label ='h0')
-ax1.plot(h1+x,label ='h1')
-#ax1.plot(h2,label ='h2')
+#x1.plot(h0+x,label ='h0')
+#ax1.plot(h1+x,label ='h1')
+ax1.plot(h2,label ='h2')
 #ax1.plot(h3+x,label ='h3')
 #ax1.plot(h4+x,label ='h4')
 #ax1.plot(h5+x,label ='h5')
